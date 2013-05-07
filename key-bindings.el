b306d886f28fb05d78caa902ece5e7fe1b361489
bendikro <bendikro@gmail.com>
2013-05-07 15:39:47 +0200

    Fixed key-bindings for find tag references

--- key-bindings.el
+++ key-bindings.el
@@ -79,12 +79,14 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;; Key bindings for gtag
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (global-set-key "\M-," 'ww-next-gtag)      ;; M-; cycles to next result, after doing M-. C-M-. or C-M-,
 (global-set-key "\M-*" 'gtags-pop-stack)   ;; M-, cycles to next result, after doing M-. C-M-. or C-M-,
 (global-set-key "\M-." 'gtags-find-tag2)    ;; M-. finds tag
-(global-set-key [(control meta .)] 'gtags-find-rtag)   ;; C-M-. find all references of tag
-(global-set-key [(control meta ,)] 'gtags-find-symbol) ;; C-M-, find all usages of symbol.
+;(global-set-key [(control meta .)] 'gtags-find-rtag)   ;; C-M-. find all references of tag
+;(global-set-key [(control meta ,)] 'gtags-find-symbol) ;; C-M-, find all usages of symbol.
+
+(global-set-key "\M--" 'gtags-find-rtag)    ;; M-- finds tag references
 
 ;; Update TAGS file with F9
 (global-set-key (kbd "<f9>") 'gtags-update-current-file)
-
