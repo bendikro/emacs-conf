9cf9e2f58f4683eb0b800ccb61bd243776f13c97
bendikro <bendikro@gmail.com>
2016-04-05 17:41:26 +0200

    Fix bug in bin/setup_config.py

--- bin/setup_config.py
+++ bin/setup_config.py
@@ -12,8 +12,6 @@ def add_to_config(filename, search_pattern, conf, create_if_not_exists=True):
             if res:
                 return
     elif not create_if_not_exists:
-        return
-    else:
         print "Nothing done on '%s'" % filename
         return
 
@@ -28,39 +26,39 @@ configs = [
     (os.path.join(home, ".tmux.conf"), "TMUX_CONF_DIR", """
 TMUX_CONF_DIR=%s/.emacs.d/configs/tmux
 source-file $TMUX_CONF_DIR/tmux.conf
-    """ % "/home/bro"),
+""" % "/home/bro", True),
     # .profile
     (os.path.join(home, ".profile"), "# Add pymacs to PYTHONPATH", """
 # Add pymacs to PYTHONPATH
 if [ -f $HOME/.emacs.d/bash/pypath_pymacs ]; then
     . $HOME/.emacs.d/bash/pypath_pymacs
 fi
-"""),
+""", True),
     # bash
 (os.path.join(home, ".bashrc"), "/.emacs.d/bash/bashrc_extras", """
 if [ -f ~/.emacs.d/bash/bashrc_extras ]; then
     . ~/.emacs.d/bash/bashrc_extras
 fi
-"""),
+""", True),
     # inputrc
 (os.path.join(home, ".inputrc"), "/.emacs.d/configs/inputrc", """
 $include ~/.emacs.d/configs/inputrc
-"""),
+""", True),
     # Git config
     (os.path.join(home, ".gitconfig"), "/.emacs.d/configs/gitconfig", """
 [include]
      path = ~/.emacs.d/configs/gitconfig
-"""),
+""", True),
     # gtk-3.0 active terminal tab color
     (os.path.join(home, ".config/gtk-3.0/gtk.css"), "TerminalWindow .notebook tab:active", """
 TerminalWindow .notebook tab:active {
     background-color: #b6bccb;
 }
-""")
+""", False)
 ]
 
 
 if __name__ == '__main__':
 
-    for fname, pattern, conf in configs:
-        add_to_config(fname, pattern, conf, True)
+    for fname, pattern, conf, create_if_not_exists in configs:
+        add_to_config(fname, pattern, conf, create_if_not_exists=create_if_not_exists)
