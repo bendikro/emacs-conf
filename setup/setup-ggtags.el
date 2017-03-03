9a232f9505fa626dbaaa189893ef53eedc1e9443
Bendik R. Opstad <bendiko@ifi.uio.no>
2017-03-03 13:07:10 +0100

    Enable ggtags-mode on c++ mode

--- setup/setup-ggtags.el
+++ setup/setup-ggtags.el
@@ -1,22 +1,19 @@
 (provide 'setup-ggtags)
-(require 'ggtags nil 'noerror)
+
+(when (require 'ggtags nil 'noerror)
+  ;; Autoload ggtags-mode on c++-mode-hook
+  (add-hook 'c++-mode-hook '(lambda () (ggtags-mode 1) ))
+  )
 
 (add-hook 'ggtags-mode-hook
 		  '(lambda()
-			 (message "gttags-mode-hook")
 			 ;; ggtags
-			 ;;(global-set-key "\M-." 'ggtags-find-tag2)    ;; M-. finds tag
-			 ;;(global-set-key "\M-," 'ggtags-find-tag-continue)    ;; M-; cycles to next result, after doing M-. C-M-. or C-M-,
-			 ;(define-key ggtags-mode-map (kbd "M-.") 'ggtags-find-tag)
-			 ;;(define-key ggtags-mode-map (kbd "M-.") 'ggtags-find-tag-dwim2)
-			 ;;(define-key ggtags-mode-map (kbd "M-.") 'ggtags-find-reference)
 			 (define-key ggtags-mode-map (kbd "M-,") 'ggtags-find-tag-continue)
 			 ;;(define-key ggtags-mode-map (kbd "C-c g h") 'ggtags-view-tag-history)
 			 (define-key ggtags-mode-map (kbd "M--") 'ggtags-find-reference)
+			 ;; Do not underline valid tags, e.g. functions
+			 (set-face-attribute 'ggtags-highlight nil :underline nil)
 			 ;;(define-key ggtags-mode-map (kbd "C-c g f") 'ggtags-find-file)
 			 ;;(define-key ggtags-mode-map (kbd "C-c g c") 'ggtags-create-tags)
 			 ;;(define-key ggtags-mode-map (kbd "C-c g u") 'ggtags-update-tags)
-			 ;;(global-set-key "\M-*" 'gtags-pop-stack)
-			 ;;(global-set-key "\M--" 'gtags-find-rtag)    ;; M-- finds tag references
-										;ggtags-next-mark
 			 ))
