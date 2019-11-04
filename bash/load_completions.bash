d3bb92c508719610a84474cb6c61fea9a1a34f4c
bendikro <bro.devel@gmail.com>
2019-11-04 19:26:01 +0100

    [bash] Fix git completion with bash-compleion lazy loading

--- bash/load_completions.bash
+++ bash/load_completions.bash
@@ -8,10 +8,15 @@ if [[ "$OS" == 'FreeBSD' ]]; then
 	fi
 fi
 
+for filename in $(find $EMACS_HOME/.emacs.d/bash/bash_completion/ -type f); do
+	. $filename
+done
+
 if [ -f /etc/bash_completion ]; then
 	. /etc/bash_completion
 fi
 
-for filename in $(find $EMACS_HOME/.emacs.d/bash/bash_completion/ -type f); do
-	. $filename
-done
+# Due to lazy loading, git has to be loaded explicitly here
+# If not loaded here, the git completion in .emacs.d/bash/bash_completion/
+# prevents the global git completion from being loaded
+_completion_loader git
