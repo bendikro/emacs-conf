bf45aa7c295790fbb3fc6df9b9eae9ee022fa2ec
bendikro <bro.devel@gmail.com>
2020-03-19 20:47:33 +0100

    [xclip] Fix clipb-copy failing for some reason

--- setup/setup-xclip.el
+++ setup/setup-xclip.el
@@ -1,15 +1,25 @@
-(require 'xclip nil 'noerror)
+(if (require 'xclip nil 'noerror)
+	(message "xclip found")
+  (message "Failed to load xclip"))
 
 (setq x-select-enable-clipboard t)
+
+(defun xclip-select-text (text)
+  "Copied from xclip.el Version: 1.10 to avoid 'void-function xclip-select-text'"
+  (xclip-set-selection 'primary text)
+  (setq xclip-last-selected-text-primary text)
+  (when xclip-select-enable-clipboard
+	(xclip-set-selection 'clipboard text)
+      (setq xclip-last-selected-text-clipboard text)))
 
 (defun clipb-copy ()
   "Copies selection to x-clipboard."
   (interactive)
   (if (display-graphic-p)
-	  (progn
-		(call-interactively 'clipboard-kill-ring-save)
-		(message "Copied region to x-clipboard")
-		)
+	(progn
+	  (call-interactively 'clipboard-kill-ring-save)
+	  (message "Copied region to x-clipboard")
+	  )
 	(if (region-active-p)
 		(let (
 			  (selection (buffer-substring-no-properties (region-beginning) (region-end))))
